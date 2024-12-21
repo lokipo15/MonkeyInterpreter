42c9851c44a7a8e221a9fc42752cb7f95169711f
Gabriel Boruń <borun.gabriel@gmail.com>
2024-12-21 14:15:23 +0100

    FEAT: Created basic lexer functionality along with helper types

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"interpreter-book/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
